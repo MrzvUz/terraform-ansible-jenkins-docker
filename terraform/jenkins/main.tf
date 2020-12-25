4adf82900b4a0cb7a8dc53ce348c222c11f48d63
mrzvuz <mrzv.uz@gmail.com>
2020-12-25 15:58:23 -0600

    updated

--- terraform/jenkins/main.tf
+++ terraform/jenkins/main.tf
@@ -12,6 +12,6 @@ resource "aws_instance" "DevOps-Jenkins" {
   key_name      = "devops"
 
   tags = {
-    Name = "aws_instance.DevOps-Jenkins"
+    Name = "DevOps-Jenkins"
   }
 }
--- terraform/web/main.tf
+++ terraform/web/main.tf
@@ -6,12 +6,12 @@ variable "name" {
   description = "Name of the web box at apply"
 }
 
-resource "aws_instance" "DevOps-Jenkins-Web" {
+resource "aws_instance" "DevOps-Web" {
   ami           = "ami-0885b1f6bd170450c"
   instance_type = "t2.micro"
   key_name      = "devops"
 
   tags = {
-    Name = "aws_instance.DevOps-Jenkins-Web"
+    Name = "DevOps-Web"
   }
 }
